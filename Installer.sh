ba3f7aea10b23b26ad6a538270d6246df3aa0361
IsCoffeeTho <aaron.menadue@gmail.com>
2021-07-25 15:49:31 +0930

    fixed echo commands

--- Installer.sh
+++ Installer.sh
@@ -1,12 +1,12 @@
 curl -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check 2>> ./install-dump
 if grep -q 'vogShip' 'vogship-check';
 then
-  echo -e "\e[97mDownloading the Vogship..."
+  echo -e '\e[97mDownloading the Vogship...'
   curl -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/commands.sh 2>> ./install-dump
   if grep -q '# vogShip v1' "vogship";
   then
-    echo -e "\e[90m---"
-    echo -e "\e[97mInstalling the Vogship..."
+    echo -e '\e[90m---'
+    echo -e '\e[97mInstalling the Vogship...'
     rm -rf ~/.zshrc
     mv -f vogship ~/.zshrc
     chmod a+x ~/.zshrc
@@ -24,11 +24,11 @@ errorPrint() {
   cat vogShip >> ./install-dump
   rm vogship
   cp install-dump download-err.log
-  echo -e "\e[90m---"
-  echo -e "\e[97mThere was a problem downloading the Vogship."
-  echo -e "\e[90m---"
-  echo -e "\e[96mIf problems persist, post an issue on the github repo."
-  echo -e "\e[96mJust remember to post your \e[91mdownload-err.log\e[96m file"
-  echo -e "\e[96mfound in this Directory.\e[0m"
-  echo -e "\e[37m(\e[94mhttps://github.com/IsCoffeeTho/42vogship/issues/new\e[37m)"
+  echo -e '\e[90m---'
+  echo -e '\e[97mThere was a problem downloading the Vogship.'
+  echo -e '\e[90m---'
+  echo -e '\e[96mIf problems persist, post an issue on the github repo.'
+  echo -e '\e[96mJust remember to post your \e[91mdownload-err.log\e[96m file'
+  echo -e '\e[96mfound in this Directory.\e[0m'
+  echo -e '\e[37m(\e[94mhttps://github.com/IsCoffeeTho/42vogship/issues/new\e[37m)'
 }
--- commands.sh
+++ commands.sh
@@ -3,22 +3,35 @@
 # Author: Aaron Menadue (amenadue)
 #         IsCoffeeTho#0001 (274012313785466881)
 
-compile() {gcc -Wall -Werror -Wextra $@;echo -e "\e[36m"}
+compile() {
+  gcc -Wall -Werror -Wextra $@
+}
 
-car() {gcc -Wall -Werror -Wextra $@ -o a;./a;rm a}
+car() {
+  gcc -Wall -Werror -Wextra $@ -o a
+  ./a
+  rm a
+  }
 
-norm() {Norminette -R CheckForbiddenSourceHeader $@}
+norm() {
+  Norminette -R CheckForbiddenSourceHeader $@
+}
 
-medir() {mkdir $1;cd $1}
+medir() {
+  mkdir $1
+  cd $1
+}
 
-cls() {clear}
+cls() {
+  clear
+}
 
 updateVogship() {
   curl -s -L -o vogship-version-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check
   if grep -q 'vogShip v1' "vogship-version-check";
   then
-    echo -e "\e[32mVogShip is already up to date!"
-    echo -e "\e[92mVersion v1 \e[97m(\e[92mBeta\e[97m)\e[0m"
+    echo -e '\e[32mVogShip is already up to date!'
+    echo -e '\e[92mVersion v1 \e[97m(\e[92mBeta\e[97m)\e[0m'
   else 
     curl -L -o Installer.sh https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/Installer.sh 2> ./install-dump
     ./Installer.sh

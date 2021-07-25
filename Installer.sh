09d5dc68a5a437a1d8a27f65dd23d27e0d65b66b
IsCoffeeTho <aaron.menadue@gmail.com>
2021-07-25 15:06:52 +0930

    Quick Fix v1

--- Installer.sh
+++ Installer.sh
@@ -1,20 +1,7 @@
 # Check version online
 curl -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check 2>> ./install-dump
 
-error-print() {
-  cat vogShip >> ./install-dump
-  rm vogship
-  cp install-dump download-err.log
-  echo -e "\e[90m---"
-  echo -e "\e[97mThere was a problem downloading the Vogship."
-  echo -e "\e[90m---"
-  echo -e "\e[96mIf problems persist, post an issue on the github repo."
-  echo -e "\e[96mJust remember to post your \e[91mdownload-err.log\e[96m file"
-  echo -e "\e[96mfound in this Directory.\e[0m"
-  echo -e "\e[37m(\e[94mhttps://github.com/IsCoffeeTho/42vogship/issues/new\e[37m)"
-}
-
-if grep -q 'vogShip v1' 'vogship-check';
+if grep -q 'vogShip' 'vogship-check';
 then
   echo -e "\e[97mDownloading the Vogship..."
   curl -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/commands.sh 2>> ./install-dump
@@ -26,10 +13,28 @@ then
     source ~/.zshrc
     rm -rf Installer.sh 
   else
-    error-print
+    cat vogShip >> ./install-dump
+    rm vogship
+    cp install-dump download-err.log
+    echo -e "\e[90m---"
+    echo -e "\e[97mThere was a problem downloading the Vogship."
+    echo -e "\e[90m---"
+    echo -e "\e[96mIf problems persist, post an issue on the github repo."
+    echo -e "\e[96mJust remember to post your \e[91mdownload-err.log\e[96m file"
+    echo -e "\e[96mfound in this Directory.\e[0m"
+    echo -e "\e[37m(\e[94mhttps://github.com/IsCoffeeTho/42vogship/issues/new\e[37m)"
   fi
 else
-  error-print
+  cat vogShip >> ./install-dump
+  rm vogship
+  cp install-dump download-err.log
+  echo -e "\e[90m---"
+  echo -e "\e[97mThere was a problem downloading the Vogship."
+  echo -e "\e[90m---"
+  echo -e "\e[96mIf problems persist, post an issue on the github repo."
+  echo -e "\e[96mJust remember to post your \e[91mdownload-err.log\e[96m file"
+  echo -e "\e[96mfound in this Directory.\e[0m"
+  echo -e "\e[37m(\e[94mhttps://github.com/IsCoffeeTho/42vogship/issues/new\e[37m)"
 fi
 
 rm -rf install-dump

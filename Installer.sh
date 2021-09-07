6b1f17dbe41a5f2e1c00072de164ca9a6f815d8a
Aaron Menadue <33318553+IsCoffeeTho@users.noreply.github.com>
2021-09-07 20:40:16 +0930

    debug

--- Installer.sh
+++ Installer.sh
@@ -3,6 +3,7 @@ echo -e '\e[90mSearching for the Vogship...'
 curl -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check
 if grep -q 'vogShip' 'vogship-check';
 then
+  echo -e '\e[92mcurling commands'
   curl -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/commands.sh 1&2>> ./install-dump
   if grep -q "# vogShip " "vogship";
   then
@@ -24,16 +25,17 @@ then
     mv vogShip install-dump
     rm vogship
     cp install-dump download-err.log
-    echo -e 'created \e[91mdownload-err.log\e[96m file'
+    echo -e '\e[96mCreated \e[91mdownload-err.log\e[96m file'
     echo -e '\e[91mThere is a problem trying to find vogShip'
   fi
 else
+  echo -e '\e[92mcurling google'
   curl -s -L http://google.com
   if [ $? -eq 0 ]; then
       mv vogShip install-dump
       rm vogship
       cp install-dump download-err.log
-      echo -e 'created \e[91mdownload-err.log\e[96m file'
+      echo -e '\e[96mCreated \e[91mdownload-err.log\e[96m file'
       echo -e '\e[91mThere is a problem trying to find vogShip,'
       echo -e '\e[91mWatchout for firewalls or proxies.'
   else

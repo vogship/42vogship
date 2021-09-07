1e0fd2ab64434ee20a90c4de1e3f0aa3152352ca
Aaron Menadue <33318553+IsCoffeeTho@users.noreply.github.com>
2021-09-07 16:30:34 +0930

    added genhead

--- commands.sh
+++ commands.sh
@@ -1,58 +1,60 @@
-#!/bin/zsh
+#!/bin/bash
 
 # vogShip v1 Beta
-# 
-# Author: Aaron Menadue    (amenadue)
+#
+# Author: Aaron Menadue (amenadue)
 #         IsCoffeeTho#0001 (274012313785466881)
 
 compile() {
-  zsh
   gcc -Wall -Werror -Wextra $@
 }
 
 car() {
-  zsh
   gcc -Wall -Werror -Wextra $@ -o a
   ./a
   rm a
 }
 
 norm() {
-  zsh
   Norminette -R CheckForbiddenSourceHeader $@
 }
 
 medir() {
-  zsh
   mkdir $1
   cd $1
 }
 
 cls() {
-  zsh
   clear
 }
 
-genmain() {
-  zsh
-  echo -e "\e[91Command not Supported"
+genhead() {
+  for f in $@;
+  do
+    if ! grep -q 'By: * <*@*>' $f; then
+      if ! grep -q 'Created: */*/* *:*:* by *' $f; then
+        if ! grep -q 'Updated: */*/* *:*:* by *' $f; then
+          vim -c 'Stdheader' -c 'wq' $f"
+        fi
+      fi
+    fi
+  done
 }
 
 updateVogship() {
-  zsh
-  curl -s -L --trace-ascii install-dump -o vogship-version-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check
+  curl -s -L -o vogship-version-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check
   if grep -q 'vogShip v1 ' "vogship-version-check";
   then
     if $1 = '-f';
     then
-      curl -s -L --trace-ascii install-dump -o Installer.sh https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/Installer.sh
+      curl -L -o Installer.sh https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/Installer.sh 2> ./install-dump
       ./Installer.sh
-    else 
+    else
       echo -e '\e[32mVogShip is already up to date!'
       echo -e '\e[92mVersion v1 \e[97m(\e[92mBeta\e[97m)\e[0m'
     fi
-  else 
-    curl -s -L --trace-ascii install-dump -o Installer.sh https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/Installer.sh
+  else
+    curl -L -o Installer.sh https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/Installer.sh 2> ./install-dump
     ./Installer.sh
   fi
 }

23f7d4846e6102f273a94b58e61b62b9ae01225b
Aaron Menadue <33318553+IsCoffeeTho@users.noreply.github.com>
2021-09-08 09:23:30 +0930

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,7 +3,6 @@ echo -e '\e[90mSearching for the Vogship...'
 curl -s -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/version-check
 if grep -q 'vogShip' 'vogship-check';
 then
-  echo -e '\e[92mcurling commands'
   curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/commands.sh
   if grep -q ".*# vogShip .*" "vogship";
   then
@@ -29,18 +28,7 @@ then
     echo -e '\e[91mThere is a problem trying to find vogShip'
   fi
 else
-  echo -e '\e[92mcurling google'
-  curl -s -L http://google.com
-  if [ $? -eq 0 ]; then
-      mv vogShip install-dump
-      rm vogship
-      cp install-dump download-err.log
-      echo -e '\e[96mCreated \e[91mdownload-err.log\e[96m file'
-      echo -e '\e[91mThere is a problem trying to find vogShip,'
-      echo -e '\e[91mWatchout for firewalls or proxies.'
-  else
-      echo -e '\e[91mCannot connect to the Internet'
-  fi
+  echo -e '\e[91mCannot connect to the Internet'
 fi
 rm -rf vogship-check
 rm -rf install-dump

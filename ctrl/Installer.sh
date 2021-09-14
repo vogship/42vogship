399991bde29ee33e5d9cadd99cbca16913f7193a
Aaron Menadue <33318553+IsCoffeeTho@users.noreply.github.com>
2021-09-14 09:41:26 +0930

    Update Installer.sh

--- ctrl/Installer.sh
+++ ctrl/Installer.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 echo -e "\e[90mSearching for the Vogship...\e[0m"
 curl -s -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/ctrl/version-check
-if grep -q 'vogShip' 'vogship-check';
-then
+if grep -q 'vogShip' 'vogship-check'; then
   curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/commands.sh
-  if grep -q ".*# vogShip .*" "vogship";
-  then
+  if grep -q ".*# vogShip .*" "vogship"; then
     echo -e "\e[90m---\[0m"
-    if grep -q "# vogShip" "~/.bashrc";
-    then
+    if [ -f "~/.bashrc" ]; then
       echo -e "\e[97mCleaning Environment...\e[0m"
-      rm -rf ~/.zshrc
+      rm -rf ~/.bashrc
     fi
-    echo -e "\e[97mPlacing...\e[0m"
+    echo -e "Placing..."
     mv -f vogship ~/.bashrc
-    echo -e "\e[97mVerifying...\e[0m"
+    echo -e "Verifying..."
     chmod a+x ~/.bashrc
-    echo -e "\e[97mInstalling the Vogship...\e[0m"
+    echo -e "Installing the Vogship..."
     source ~/.bashrc
-    echo -e "\e[97mInstallation Complete.\e[0m"
+    echo -e "Installation Complete."
   else
     mv vogShip install-dump
     rm vogship
     cp install-dump download-err.log
-    echo -e "\e[96mCreated \e[91mdownload-err.log\e[96m file\e[0m"
-    echo -e "\e[91mThere is a problem trying to find vogShip\e[0m"
+    echo -e "Created download-err.log file"
+    echo -e "There is a problem trying to find vogShip"
   fi
 else
   echo -e "\e[91mCannot connect to the Internet\e[0m"

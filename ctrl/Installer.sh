a110c90aad5606f42afc40aa7acd1433b4ba0d8a
Aaron Menadue <amenadue@F2N011.42adel.org.au>
2021-10-01 13:10:43 +0930

    updated Installer to make vogship independant from

--- ctrl/Installer.sh
+++ ctrl/Installer.sh
@@ -2,33 +2,38 @@
 echo -e "Searching for the Vogship..."
 curl -s -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/ctrl/version-check
 if grep -q 'vogShip' 'vogship-check'; then
-  curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/vogship.sh
-  if grep -q ".*# vogShip .*" "vogship"; then
-    echo -e "---"
-    if [ -f ~/.zshrc ]; then
-      echo -e "adding zsh requirements..."
-      if ! grep -qE "((source)|(\.) (.*\.bashrc))" "~/.zshrc"; then
-        echo "source ~/.bashrc" >> ~/.zshrc
-      fi
-    fi
-    if [ -f "~/.bashrc" ]; then
-      echo -e "Cleaning Environment..."
-      rm -rf ~/.bashrc
-    fi
-    mv -f vogship ~/.bashrc
-    chmod a+x ~/.bashrc
-    echo -e "Installation Complete."
-    echo -e "To finish the install Please run: "
-    echo -e "source ~/.bashrc"
-  else
-    mv vogShip install-dump
-    rm vogship
-    cp install-dump download-err.log
-    echo -e "Created download-err.log file"
-    echo -e "There is a problem trying to find vogShip"
-  fi
+	curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/vogship.sh
+	if grep -q ".*# vogShip .*" "vogship"; then
+		echo -e "---"
+		if [ -f "~/.zshrc" ]; then
+			echo -e "Petting Doggo..."
+	  		if ! grep -qE "((source) (.*\.vogship\/vogship\.sh))" "~/.zshrc"; then
+				echo "source ~/.vogship/vogship.sh" >> ~/.zshrc
+	  		fi
+		else
+			echo "source ~/.vogship/vogship.sh" >> ~/.zshrc
+		fi
+		if [ -f "~/.bashrc" ]; then
+			echo -e "Feeding the Dino..."
+	  		if ! grep -qE "((source) (.*\.vogship\/vogship\.sh))" "~/.bashrc"; then
+				echo "source ~/.vogship/vogship.sh" >> ~/.bashrc
+	  		fi
+		else
+			echo "source ~/.vogship/vogship.sh" >> ~/.bashrc
+		fi
+		mv -f vogship ~/.vogship/vogship.sh
+		chmod a+x ~/.vogship/vogship.sh
+		source ~/.vogship/vogship.sh
+		echo -e "Installation Complete."
+	else
+		mv vogShip install-dump
+		rm vogship
+		cp install-dump download-err.log
+		echo -e "Created download-err.log file"
+		echo -e "There is a problem trying to find vogShip"
+	fi
 else
-  echo -e "Cannot connect to the Internet"
+	echo -e "Cannot connect to the Internet"
 fi
 rm -rf vogship-check
 rm -rf install-dump

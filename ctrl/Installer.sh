aff0b3ede099dd0a74dd5c414b463b1afc87e358
Aaron Menadue <amenadue@F2N010.42adel.org.au>
2021-10-14 12:16:02 +1030

    Added Color and Logs to Installer

--- ctrl/Installer.sh
+++ ctrl/Installer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo -e "Searching for the Vogship..."
-curl -s -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/ctrl/version-check
+printf "Searching for the \e[92mVogship\e[0m...\n"
+printf "check version: " > vogship.log
+curl -s -L -o vogship-check https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/ctrl/version-check > vogship.log 2> vogship.log
 if grep -q 'vogShip' 'vogship-check'; then
-	curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/vogship.sh
+	printf "Download vogship: " > vogship.log
+	curl -s -L -o vogship https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/vogship.sh > vogship.log 2> vogship.log
 	if grep -q ".*# vogShip .*" "vogship"; then
-		echo -e "---"
 		if [ -f "~/.zshrc" ]; then
-			echo -e "Petting Doggo..."
+			printf "Petting Doggo..."
 	  		if ! grep -qE "((source) (.*\.vogship\/vogship\.sh))" "~/.zshrc"; then
 				echo "source ~/.vogship/vogship.sh" >> ~/.zshrc
 	  		fi
@@ -14,36 +15,39 @@ if grep -q 'vogShip' 'vogship-check'; then
 			echo "source ~/.vogship/vogship.sh" >> ~/.zshrc
 		fi
 		if [ -f "~/.bashrc" ]; then
-			echo -e "Feeding the Dino..."
+			printf "Feeding the Dino..."
 	  		if ! grep -qE "((source) (.*\.vogship\/vogship\.sh))" "~/.bashrc"; then
 				echo "source ~/.vogship/vogship.sh" >> ~/.bashrc
 	  		fi
 		else
 			echo "source ~/.vogship/vogship.sh" >> ~/.bashrc
 		fi
-		mkdir ~/.vogship > /dev/null 2> /dev/null
-		mv -f vogship ~/.vogship/vogship.sh
+		printf "Make .vogship foler: " > vogship.log
+		mkdir ~/.vogship > vogship.log 2> vogship.log
+		printf "Move downloaded vogship to .vogship foler: " > vogship.log
+		mv -f vogship ~/.vogship/vogship.sh > vogship.log 2> vogship.log
 		chmod a-w ~/.vogship/vogship.sh
 		chmod a+rx ~/.vogship/vogship.sh
 		source ~/.bashrc
 		source ~/.zshrc
 		source ~/.vogship/vogship.sh
-		echo "Vogship is almost ready to lift off."
-		echo "Installing man pages."
-		curl -s -L -o ~/.vogship/man.tar https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/man.tar
-		tar -xf ~/.vogship/man.tar -C ~/.vogship/
+		printf "\e[3m\e[97mAlmost\e[0m ready to lift off..."
+		printf "Downloading Man Archive: " > vogship.log
+		curl -s -L -o ~/.vogship/man.tar https://raw.githubusercontent.com/IsCoffeeTho/42vogship/master/src/man.tar > vogship.log 2> vogship.log
+		printf "Decompress Man Archive: " > vogship.log
+		tar -xf ~/.vogship/man.tar -C ~/.vogship/ > vogship.log 2> vogship.log
 		rm -rf ~/.vogship/man.tar
-		echo "Installation Complete."
+		printf "Ready to \e[1m\e[92mFly\e[0m Through!"
+		rm -rf ~/.vogship/latest-install.log
+		mv vogship.log ~/.vogship/latest-install.log
 	else
-		mv vogship install-dump 2> /dev/null
 		rm vogship
-		cp install-dump download-err.log
-		echo -e "Created download-err.log file"
-		echo -e "There is a problem trying to find vogShip"
+		mv vogship.log download-err.log
+		printf "Generated \e[91mvogship.log\e[0m"
+		printf "There was a problem downloading VogShip, please check connection and try again."
 	fi
 else
-	echo -e "Cannot connect to the Internet"
+	printf "\e[91mCannot connect to the Internet\e[0m"
 fi
 rm -rf vogship-check
-rm -rf install-dump
 rm -rf $0
